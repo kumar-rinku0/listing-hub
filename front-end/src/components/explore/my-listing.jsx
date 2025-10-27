9ec56deb82318bb06afb8a954f4c48472deea209
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-10-28 02:19:39 +0530

    card style updated & bugs resolved

--- front-end/src/components/explore/my-listing.jsx
+++ front-end/src/components/explore/my-listing.jsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useAuth } from '../../AuthProvider';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../AuthProvider";
 
-import Skeleton from '@mui/material/Skeleton';
-import Card from './card';
+import Skeleton from "@mui/material/Skeleton";
+import Card from "./card";
 
 const MyListing = () => {
   const { isAuthenticated, user } = useAuth();
@@ -11,39 +11,55 @@ const MyListing = () => {
   const [listings, setListings] = useState(null);
   useEffect(() => {
     if (isAuthenticated) {
-      axios.get(`/api/listings/user/${user.username}`).then((res) => {
-        console.log(res.data);
-        setListings(res.data.listings);
-        setLoading(false);
-      }).catch((err) => {
-        console.error(err.response.data);
-      })
+      axios
+        .get(`/api/listings/user/${user.username}`)
+        .then((res) => {
+          console.log(res.data);
+          setListings(res.data.listings);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.error(err.response.data);
+        });
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated]);
   return (
     <div>
       {loading ? ( // Conditionally render based on loading state
-        <div className='explore'>
-          <div className='explore-card-container'>
+        <div className="explore">
+          <div className="explore-card-container">
             <Skeleton variant="rounded" width="20rem" height="15rem" />
             <Skeleton variant="rounded" width="20rem" height="15rem" />
             <Skeleton variant="rounded" width="20rem" height="15rem" />
             <Skeleton variant="rounded" width="20rem" height="15rem" />
             <Skeleton variant="rounded" width="20rem" height="15rem" />
-            {/* <Skeleton variant="rounded" width="20rem" height="15rem" /> */}
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
+            <Skeleton variant="rounded" width="20rem" height="15rem" />
           </div>
         </div>
       ) : (
-        <div className='explore'>
-          <div>
-            Listing Count: {listings.length}
-          </div>
+        <div className="explore">
           <div className="explore-card-container">
-            {Array.isArray(listings) && listings.length > 0 && listings.map((listing) => (
-              <div key={listing._id}>
-                <Card listing={listing} />
-              </div>
-            ))}
+            {Array.isArray(listings) &&
+              listings.length > 0 &&
+              listings.map((listing) => (
+                <div key={listing._id}>
+                  <Card listing={listing} />
+                </div>
+              ))}
             {Array.isArray(listings) && listings.length === 0 && (
               <div>No listings found.</div>
             )}
@@ -51,7 +67,7 @@ const MyListing = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default MyListing;
